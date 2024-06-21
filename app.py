f6b51e95eb2bf4701a940e4c7fa188020ac8195e
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-06-21 10:10:42 +0900

    remove relationship

--- app.py
+++ app.py
@@ -106,29 +106,15 @@ def select_heroes():
 def update_heroes():
     with Session(engine) as session:
         statement = select(Hero).where(Hero.name == "Spider-Boy")
-        results = session.exec(statement)
-        hero_1 = results.one()
-        print("Hero 1:", hero_1)
+        result = session.exec(statement)
+        hero_spider_boy = result.one()
 
-        statement = select(Hero).where(Hero.name == "Captain North America")
-        results = session.exec(statement)
-        hero_2 = results.one()
-        print("Hero 2:", hero_2)
-
-        hero_1.age = 16
-        hero_1.name = "Spider-Youngster"
-        session.add(hero_1)
-
-        hero_2.name = "Captain North America Except Canada"
-        hero_2.age = 110
-        session.add(hero_2)
-
+        hero_spider_boy.team = None
+        session.add(hero_spider_boy)
         session.commit()
-        session.refresh(hero_1)
-        session.refresh(hero_2)
 
-        print("Updated hero 1:", hero_1)
-        print("Updated hero 2:", hero_2)
+        session.refresh(hero_spider_boy)
+        print(f"{hero_spider_boy.name} without team: {hero_spider_boy} ")
 
 
 def delete_heroes():
@@ -154,7 +140,8 @@ def delete_heroes():
 def main():
     create_db_and_tables()
     create_heroes()
-    select_heroes()  # update_heroes()
+    select_heroes()
+    update_heroes()
     # delete_heroes()
 
 
