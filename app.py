880849cf05d2fc07b33ba9909cd301cbb05a016f
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-06-21 15:52:58 +0900

    update hero_team_links

--- app.py
+++ app.py
@@ -91,7 +91,32 @@ def create_heroes():
 
 def update_heroes():
     with Session(engine) as session:
-        print(session)
+        hero_spider_boy = session.exec(
+            select(Hero).where(Hero.name == "Spider-Boy")
+        ).one()
+        team_z_force = session.exec(select(Team).where(Team.name == "Z-Force")).one()
+
+        spider_boy_z_force_link = HeroTeamLink(
+            team=team_z_force,
+            hero=hero_spider_boy,
+            is_training=True,
+        )
+        team_z_force.hero_links.append(spider_boy_z_force_link)
+        session.add(team_z_force)
+        session.commit()
+
+        print(f"Updated Spider-Boy's Teams: {hero_spider_boy.team_links}")
+        print(f"Updated Z-Force's Heroes: {team_z_force.hero_links}")
+
+        for link in hero_spider_boy.team_links:
+            if link.team.name == "Preveneters":
+                link.is_training = False
+
+        session.add(hero_spider_boy)
+        session.commit()
+
+        for link in hero_spider_boy.team_links:
+            print(f"Spider-Boy teem: {link.team}, is_training: {link.is_training}")
 
 
 def main():
